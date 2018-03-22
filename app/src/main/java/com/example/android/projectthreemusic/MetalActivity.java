07dfcbbd407859ff75423775adb569508efa8bd9
Gareth Vermeulen <radgareth@gmail.com>
2018-03-22 14:46:13 +0000

    adding play button functionality to metalActivity, breaks app

--- app/src/main/java/com/example/android/projectthreemusic/MetalActivity.java
+++ app/src/main/java/com/example/android/projectthreemusic/MetalActivity.java
@@ -4,17 +4,23 @@ package com.example.android.projectthreemusic;
  * Created by gp on 15/03/2018.
  */
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 
 
-
-
 public class MetalActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,12 @@ public class MetalActivity extends AppCompatActivity {
 
 
         ArrayList<Music> songs = new ArrayList<Music>();
+
         songs.add(new Music("Iron Maiden", "Fear of the Dark"));
-        songs.add(new Music("Pantera", "Cemetery Gates") );
+        songs.add(new Music("Pantera", "Cemetery Gates"));
         songs.add(new Music("Metallica", "Enter Sandman"));
         songs.add(new Music("Guns 'N Roses", "Welcome to the Jungle"));
-        songs.add(new Music("Tool","Schism"));
+        songs.add(new Music("Tool", "Schism"));
 
 
         MusicAdapter adapter = new MusicAdapter(this, songs);
@@ -36,13 +43,25 @@ public class MetalActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
+        /**
+         * This code crashes the app
+         */
+        ImageButton playVar = (ImageButton) findViewById(R.id.play_butt);
+        playVar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MetalActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+/**
+ * Leave these two braces here
+ */
     }
 
-}
-
-
-
-
+    }
 
 
 
